457101e82ec56d82ffd939b55f05ace78edf403e
Ali-apple <aitov.74@list.ru>
2020-11-05 19:12:08 -0500

    new changes added

--- src/ClassOne.java
+++ src/ClassOne.java
@@ -1,4 +1,8 @@
 
 public class ClassOne {
-
+public static void main(String[] args) {
+System.out.println("Hello");
+System.out.println("Hello Alibay");
+System.out.println("Hello Alibay Aitov");
+}
 }
